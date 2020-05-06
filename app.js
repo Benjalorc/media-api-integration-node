9847ac522130d2106c8e174e650ebcaa4cbd41cb
Benjalorc <benjalorc@gmail.com>
2020-05-06 19:34:40 -0400

    Configuración de cors para recibir peticiones de entorno de desarrollo local en el puerto 4200

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var mediaRouter = require('./routes/media');
 
+app.use((req, res, next)=> {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', mediaRouter);
 
 app.listen(3000, ()=> {
