1d323cb933d5d59cc9613453e7a2f084e55fe5fa
Benjalorc <benjalorc@gmail.com>
2020-05-07 01:13:41 -0400

    Filtrado de estructura de resultados de servicios externos

--- routes/media.js
+++ routes/media.js
@@ -5,29 +5,78 @@ const request = require('request');
 
 router.get('/media/:param', (req, res, next)=> {
 
-	let data = req.params.param;
-	let respuesta = {
-		itunes: null,
-		tvmaze: null,
-		crc: null
-	};
-	getRest("http://api.tvmaze.com/search/shows?q=", data).then((cadena)=>{
-		if(cadena.status) cadena.data = JSON.parse(cadena.data);
-		respuesta.tvmaze = cadena;
-		if(!isEmpty(respuesta)) res.send(respuesta);
-	});
-	getRest("https://itunes.apple.com/search?term=", data).then((cadena)=>{
-		if(cadena.status) cadena.data = JSON.parse(cadena.data);
-		respuesta.itunes = cadena;
-		if(!isEmpty(respuesta)) res.send(respuesta);
-	});
-	getRest("http://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=QueryByName&name=", data).then((xml)=>{
-		if(xml.status){
-			let json = JSON.parse(parser.toJson(xml.data));
-			xml.data = json["SOAP-ENV:Envelope"]["SOAP-ENV:Body"].QueryByNameResponse.QueryByNameResult["diffgr:diffgram"].QueryByName_DataSet.QueryByName;
+	let data = req.params.param.replace(/\s/g, '+');
+
+	let consultas = [
+		{
+			origen: "itunes",
+			tipo: "json",
+			urls: [
+				"https://itunes.apple.com/search?entity=song&term=",
+				"https://itunes.apple.com/search?entity=movie&term=",
+				"https://itunes.apple.com/search?entity=ebook&term=",
+			],
+			match: "trackName",
+			respuestas: []
+		},
+		{
+			origen: "tvmaze",
+			tipo: "json",
+			urls: [
+				"http://api.tvmaze.com/search/shows?q="
+			],
+			match: "name",
+			respuestas: []
+		},
+		{
+			origen: "crc",
+			tipo: "xml",
+			urls: [
+				{
+					ruta: "http://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=QueryByName&name=",
+					obj: ['SOAP-ENV:Envelope','SOAP-ENV:Body','QueryByNameResponse','QueryByNameResult','diffgr:diffgram','QueryByName_DataSet','QueryByName']
+				}
+			],
+			match: "Name",
+			respuestas: []
 		}
-		respuesta.crc = xml;
-		if(!isEmpty(respuesta)) res.send(respuesta);
+	];
+
+	let i = 0, j = 0;
+	consultas.forEach((consulta)=>{
+		consulta.urls.forEach((url)=>{
+
+			j++;
+
+			getRest(url.ruta || url, data).then((cadena)=>{
+
+				i++;
+
+				if(cadena.status){
+
+					let data, json;
+					if(consulta.tipo === "xml"){
+						data = parseXmlData(cadena.data, url.obj);
+					}
+					else{
+						json = JSON.parse(cadena.data);
+						data = json.results || json;
+						if(data[0] && data[0].show) data = data.map((el)=>{return {...el.show, score: el.score} });
+					}
+					cadena.data = data.map((el)=>{return {id: ( el.trackId || el.id || parseInt(el.ID)), name: el[consulta.match], origen: consulta.origen}});
+				}
+
+				consulta.respuestas = [...consulta.respuestas, ...(cadena.data)];
+
+				if(i === j){
+					let extracto = consultas.map((el)=>{return el.respuestas});
+					let arreglo = mixAndSort(extracto);
+					res.send(arreglo);
+				}
+
+			});
+
+		});
 	});
 
 });
@@ -37,7 +86,6 @@ function getRest(base, param) {
 	return new Promise((resolve, reject)=>{
 
 		let ruta = base+param;
-
 		request.get(ruta, (err, res, body) => {
 			if (err) {
 				resolve({status: false, data: null, msg: "No se pudo completar la búsqueda"});
@@ -48,14 +96,22 @@ function getRest(base, param) {
 	});
 }
 
-function isEmpty(obj){
-
-	let empty = false;
-	for(let prop in obj){
-		if(obj[prop] === null){
-			empty = true; break;
-		}
-	}
-	return empty;
+function parseXmlData(xml, paths) {
+	let json = JSON.parse(parser.toJson(xml));
+	paths.forEach((path)=> json = json[path] );
+	return json || [];
 }
+
+function mixAndSort(arreglo){
+
+	let arr = [];
+	arreglo.forEach((el)=> arr.push(...el));
+
+	return arr.sort((a, b)=> {
+	  if (a.name > b.name) return 1;
+	  if (a.name < b.name) return -1;
+	  return 0;
+	});
+}
+
 module.exports = router;
